9ca52d47f887648066d6c0ea303a4b077d0aa2f8
aquamagic9 <84334547+aquamagic9@users.noreply.github.com>
2022-06-03 20:01:48 +0900

    Add WoodBoxMission

---
+++ Assets/JY/Scripts/TriggerArea.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TriggerArea : XRBaseInteractor
+{
+    private XRBaseInteractable currentInteractable = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SetInteractable(other);
+    }
+
+    private void SetInteractable(Collider other)
+    {
+        if(TryGetInteractable(other, out XRBaseInteractable interactable))
+        {
+            if (currentInteractable == null)
+                currentInteractable = interactable;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ClearInteractable(other);
+    }
+
+    private void ClearInteractable(Collider other)
+    {
+        if (TryGetInteractable(other, out XRBaseInteractable interactable))
+        {
+            if (currentInteractable == null)
+                currentInteractable = interactable;
+        }
+    }
+
+    private bool TryGetInteractable(Collider collider, out XRBaseInteractable interactable)
+    {
+        interactable = interactionManager.GetInteractableForCollider(collider);
+        return interactable != null;
+    }
+
+    public override void GetValidTargets(List<XRBaseInteractable> validTargets)
+    {
+        validTargets.Clear();
+        validTargets.Add(currentInteractable);
+    }
+
+    public override bool CanHover(XRBaseInteractable interactable)
+    {
+        return base.CanHover(interactable) && currentInteractable == interactable;
+    }
+
+    public override bool CanSelect(XRBaseInteractable interactable)
+    {
+        return false;
+    }
+}
