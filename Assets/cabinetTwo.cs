2042a1a5ee19d656a7d959f5ce828957165c6120
aquamagic9 <84334547+aquamagic9@users.noreply.github.com>
2022-06-06 21:20:23 +0900

    Add mission in 3x4 cabinet

---
+++ Assets/cabinetTwo.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cabinetTwo : MonoBehaviour
+{
+    bool ani = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ani)
+        {
+            transform.Rotate(new Vector3(0, -90, 0) * Time.deltaTime);
+            if (transform.rotation.y >= 0.3)
+            {
+                ani = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "MouseCube")
+        {
+            ani = true;
+        }
+    }
+}
